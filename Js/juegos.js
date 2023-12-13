0aa1f6cad4c9e7178abb7754c5205a85b58b49aa
mariano202 <marianoaarias202@gmail.com>
2023-12-12 23:28:20 -0300

    pagina juegos deseado terminada

--- Js/juegos.js
+++ Js/juegos.js
@@ -31,14 +31,35 @@ const renderizarJuegosEnHTML = () => {
     const idBoton = `verDetalles-${juego.id}`;
     const nuevoElementoJuego = document.createElement('div');
     nuevoElementoJuego.innerHTML = `
-      <div class="card mb-3" style="width: 100%;">
-        <img src="${juego.imagen}" class="card-img-top" alt="${juego.titulo}">
-        <div class="card-body">
-          <h5 class="card-title">${juego.titulo}</h5>
-          <a href="#" id='${idBoton}' class="btn btn-primary">Ver Detalles</a>
-        </div>
+
+      <div class="col-md-6 col-lg-3 mb-4">
+      <div class="card text-center bg-black py-3 h-100 card-top-red">
+          <div class="img-container-product">
+              <div class="icons-container">
+                  <span><i class="fa-solid fa-heart " aria-hidden="true"></i></span>
+                  <span><i class="fa-solid fa-cart-shopping " aria-hidden="true"></i></span>
+              </div>
+              <img src="${juego.imagen}" class="card-img-top" alt="imagen de juego">
+          </div>
+          <div class="info-container-product">
+          
+              <h3 class="card-title text-light tituloJuego">${juego.titulo}</h3>
+            <p class="card-text colorLetras h4 tamañoPalabras">${juego.descripcion}</p>
+            <div class="mb-md-5 mb-lg-5">
+              <p class="colorLetras h6">Categoria: ${juego.categoria}</p>
+              <p class="colorLetras h6">Desarrolador: ${juego.desarrollador}</p>
+              <p class="colorLetras mb-lg-5 h6"> <i class="bi bi-microsoft"></i></i></p>
+            </div>
+              <p class="price fs-3">$${juego.precio} <span>$${ (juego.precio-(juego.precio * 0.20)).toFixed(2)  }</span></p>
+              <div class="mt-2">
+             
+                  <a href="#" id='${idBoton}' class="btn-customized">Ver Detalles</a>
+              </div>
+          </div>
       </div>
-    `;
+  </div>
+    
+      `;
 
     // Agregar evento click al botón
     const botonVerDetalles = nuevoElementoJuego.querySelector(`#${idBoton}`);
--- Js/juegosDesados.js
+++ Js/juegosDesados.js
@@ -15,12 +15,32 @@ const cargarJuegosDeseados = () => {
   contenedor.innerHTML = ''
   lista._lista.map(juego => {
     contenedor.innerHTML += `
-    <div class="card mb-3" style="width: 100%;">
-        <img src="${juego.imagen}" class="card-img-top img-fluid w-50" alt="${juego.titulo}">
-        <div class="card-body">
-          <h5 class="card-title">${juego.titulo}</h5>
+    <div class="col-md-6 col-lg-3 mb-4">
+    <div class="card text-center bg-black py-3 h-100 card-top-red">
+        <div class="img-container-product">
+            <div class="icons-container">
+                <span><i class="fa-solid fa-heart " aria-hidden="true"></i></span>
+                <span><i class="fa-solid fa-cart-shopping " aria-hidden="true"></i></span>
+            </div>
+            <img src="${juego.imagen}" class="card-img-top" alt="imagen de juego">
         </div>
-      </div>`
+        <div class="info-container-product">
+        
+            <h3 class="card-title text-light tituloJuego">${juego.titulo}</h3>
+          <p class="card-text colorLetras h4 tamañoPalabras">${juego.descripcion}</p>
+          <div class="mb-md-5 mb-lg-5">
+            <p class="colorLetras h6">Categoria: ${juego.categoria}</p>
+            <p class="colorLetras h6">Desarrolador: ${juego.desarrollador}</p>
+            <p class="colorLetras mb-lg-5 h6"> <i class="bi bi-microsoft"></i></i></p>
+          </div>
+            <p class="price fs-3">$${juego.precio} <span>$${ (juego.precio-(juego.precio * 0.20)).toFixed(2)  }</span></p>
+            <div class="mt-2">
+                <a href="../Pages/error404.html" class="btn-customized">Ver más</a>
+            </div>
+        </div>
+    </div>
+</div>
+    `
   })
 }
 
